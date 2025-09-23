75aac2a826a801cb3d31222baa640e64dd0ea277
JheralMaquera <jjmaqueral@unjbg.edu.pe>
2025-09-22 22:35:06 -0500

    feat: implement insert function in UserModel

--- playground.js
+++ playground.js
@@ -1,6 +1,25 @@
 import { UserModel } from './models/user/userModel.js';
 import pool from './db/db.js';
-const result = await UserModel.login('2023-119018', 'vaquitaMarina1234');
-console.log(result);
 
-pool.end();
+try {
+  const userData = {
+    userCode: 'U005',
+    firstName: 'María',
+    lastName: 'Gómez',
+    dni: '87632d31',
+    email: 'mar1aiaa@example.com',
+    role: 'estudiante',
+    passwordHash: 'clave123',
+  };
+
+  const result = await UserModel.insert(userData);
+  console.log('Resultado insert:', result);
+
+  const res = await pool.query('SELECT * FROM usuarios');
+  console.log('Usuarios en la BD:');
+  console.log(res.rows);
+} catch (error) {
+  console.error('Error en playground:', error);
+} finally {
+  await pool.end();
+}
