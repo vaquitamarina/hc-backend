71d571657695d573e252bba7be1b68b61c31c6de
vaquita <cristhianvm55@gmail.com>
2025-09-23 19:28:22 -0500

    struct

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
 import { userRoutes } from './userRoutes.js';
+import { pacientRoutes } from './pacientRoutes.js';
+import { hcRoutes } from './hcRoutes.js';
 
 export const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/pacient', pacientRoutes);
+router.use('/hc', hcRoutes);
